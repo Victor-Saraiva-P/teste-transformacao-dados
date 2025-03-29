9334baac297a754f879119122c67516fd6490dca
Victor Saraiva <victor_alex2005@hotmail.com>
2025-03-29 14:57:51 -0300

    feat: atualiza a página final a ser processada no arquivo de configuração

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ NOME_ARQUIVO_COMPACTADO = "Teste_Victor_Alexandre_Saraiva_Pimentel"  # Nome base
 
 NOME_ARQUIVO_PDF = "Anexo_I.pdf"  # Nome do arquivo PDF a ser transformado
 PAGINA_INICIAL = 3  # Página inicial a ser processada
-PAGINA_FINAL = 5  # Página final a ser processada
+PAGINA_FINAL = 181  # Página final a ser processada
 
 # Cabeçalho oficial esperado (13 colunas)
 HEADER_CSV = [
